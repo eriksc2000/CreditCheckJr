4918acdf66c3ff77a3c77af230dc8f9482a61bbe
Erik Schneider <erikschneider2211@gmail.com>
2018-06-27 21:16:14 -0600

    5 out of 6 iterations run

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,26 +1,22 @@
 card_number = "4929735477250543"
 
 class CreditCheck 
-
-  def initialize(card_number)
-    @card_number = card_number
+    
+  def change_to_array(card_number)
+    card_number.to_i.digits
   end
   
-  def change_to_array 
-    @card_number.to_i.digits
-  end
-  
-  def doubles_odd_numbers
+  def doubles_odd_numbers(change_to_array)
     change_to_array.map.with_index do |num, index|
-    if index.odd?
-      num * 2
-    else
-      num
+      if index.odd?
+        num * 2
+      else
+        num
+      end
     end
-  end
   end 
   
-  def adds_numbers_greater_than_9
+  def adds_numbers_greater_than_9(doubles_odd_numbers)
     sum_greater_than_9 = doubles_odd_numbers.map do |number|
       if number > 9
         number - 9
@@ -28,18 +24,26 @@ class CreditCheck
         number 
       end 
     end 
-    end 
+  end 
   
-  def totals_entire_number 
+  def totals_entire_number(adds_numbers_greater_than_9) 
     adds_numbers_greater_than_9.sum 
   end 
 
-  def is_card_valid
+  def valid_number?(totals_entire_number)
     if totals_entire_number % 10 == 0
-      print "The number is valid!"
+       true
     else 
-      print "The number is invalid!"
+       false
     end 
   end 
+  
+  # def validation_output(card_number)
+  #   if totals_entire_number % 10 == 0
+  #     "The number #{card_number} is valid."
+  #   else 
+  #     "The number #{card_number} is invalid."
+  #   end 
+  # end 
   
 end
---
+++ test/credit_check_test.rb
@@ -0,0 +1,52 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/credit_check"
+
+class CreditCheckTest < Minitest::Test 
+  
+  def test_it_exists
+    credit_check = CreditCheck.new
+    
+    assert_instance_of CreditCheck, credit_check
+  end
+  
+  def test_it_changes_to_array_and_reverses
+    credit_check = CreditCheck.new
+    credit_card = 4929735477250543
+    
+    expected = [3,4,5,0,5,2,7,7,4,5,3,7,9,2,9,4]
+    assert_equal expected, credit_check.change_to_array(credit_card)
+    end
+  
+  def test_it_can_double_odd_numbers
+    credit_check = CreditCheck.new
+    credit_card = [3,4,5,0,5,2,7,7,4,5,3,7,9,2,9,4]
+    
+    expected = [3,8,5,0,5,4,7,14,4,10,3,14,9,4,9,8]
+    assert_equal expected, credit_check.doubles_odd_numbers(credit_card)
+    end
+  
+   def test_it_adds_numbers_greater_than_9
+    credit_check = CreditCheck.new
+    credit_card = [3,8,5,0,5,4,7,14,4,10,3,14,9,4,9,8]
+    
+    expected = [3,8,5,0,5,4,7,5,4,1,3,5,9,4,9,8]
+    assert_equal expected, credit_check.adds_numbers_greater_than_9(credit_card)  
+   end
+  
+  def test_it_totals_entire_number
+    credit_check = CreditCheck.new
+    credit_card = [3,8,5,0,5,4,7,5,4,1,3,5,9,4,9,8]
+    
+    assert_equal 80, credit_check.totals_entire_number(credit_card)
+  end
+  
+  def test_valid_number?
+    credit_check = CreditCheck.new
+  
+    assert_equal true, credit_check.valid_number?(4929735477250543)
+  end 
+
+end
+  
+  
