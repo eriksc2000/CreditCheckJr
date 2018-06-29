a1b1dbcf8cbfe99b5bd7e71779a155647e337be7
Erik Schneider <erikschneider2211@gmail.com>
2018-06-28 19:52:47 -0600

    change map methods to each

--- lib/credit_check.rb
+++ lib/credit_check.rb
@@ -1,31 +1,45 @@
-card_number = "4929735477250543"
+require 'pry'
 
 class CreditCheck 
+  
+  
+  # def something(number)
+  #   changed = change_to_array(number)
+  #   dbl = doubles_odd_numbers(changed)
+  #   added = adds_numbers_greater_than_9(dbl)
+  #   totals = totals_entire_number(added)
+  #   valid = valid_number?(totals)
+  #   binding.pry
+  # end
     
   def change_to_array(card_number)
     card_number.to_i.digits
   end
-  
+ 
   def doubles_odd_numbers(change_to_array)
-    change_to_array.map.with_index do |num, index|
+    doubled = []
+    change_to_array.each.with_index do |num, index|
       if index.odd?
-        num * 2
-      else
-        num
-      end
-    end
+        doubled << num * 2
+      else 
+        doubled << num 
+      end 
+    end 
+    doubled 
   end 
   
   def adds_numbers_greater_than_9(doubles_odd_numbers)
-    sum_greater_than_9 = doubles_odd_numbers.map do |number|
+    sum_greater_than_9 = []
+    doubles_odd_numbers.each do |number|
       if number > 9
-        number - 9
+        sum_greater_than_9 << number - 9
       else
-        number 
+        sum_greater_than_9 << number 
       end 
     end 
+    sum_greater_than_9
   end 
-  
+
   def totals_entire_number(adds_numbers_greater_than_9) 
     adds_numbers_greater_than_9.sum 
   end 
@@ -37,13 +51,14 @@ class CreditCheck
        false
     end 
   end 
-  
-  # def validation_output(card_number)
-  #   if totals_entire_number % 10 == 0
-  #     "The number #{card_number} is valid."
-  #   else 
-  #     "The number #{card_number} is invalid."
-  #   end 
-  # end 
-  
+
+  def validation_output(valid_number)
+    if totals_entire_number % 10 == 0
+      "The number #{card_number} is valid."
+    else 
+      "The number #{card_number} is invalid."
+    end 
+  end 
+
 end
+
--- test/credit_check_test.rb
+++ test/credit_check_test.rb
@@ -21,10 +21,10 @@ class CreditCheckTest < Minitest::Test
   def test_it_can_double_odd_numbers
     credit_check = CreditCheck.new
     credit_card = [3,4,5,0,5,2,7,7,4,5,3,7,9,2,9,4]
-    
+  
     expected = [3,8,5,0,5,4,7,14,4,10,3,14,9,4,9,8]
     assert_equal expected, credit_check.doubles_odd_numbers(credit_card)
-    end
+  end
   
    def test_it_adds_numbers_greater_than_9
     credit_check = CreditCheck.new
@@ -41,12 +41,32 @@ class CreditCheckTest < Minitest::Test
     assert_equal 80, credit_check.totals_entire_number(credit_card)
   end
   
-  def test_valid_number?
+  def test_valid_number_is_true
     credit_check = CreditCheck.new
   
-    assert_equal true, credit_check.valid_number?(4929735477250543)
+    assert_equal true, credit_check.valid_number?(80)
   end 
-
+  
+  def test_valid_number_is_false
+    credit_check = CreditCheck.new
+    
+    assert_equal false, credit_check.valid_number?(81)
+  end 
+  
+  def test_validation_output_is_correct
+    credit_check = CreditCheck.new 
+    
+    expected = "The number 4929735477250543 is valid."
+    assert_equal expected, credit_check.validation_output(80)
+  end 
+  
+  def test_validation_output_is_incorrect
+    skip
+    credit_check = CreditCheck.new 
+    
+    assert_equal "The number #{card_number} is invalid", credit_check.valid_number(81)
+  end 
+  
 end
   
   
